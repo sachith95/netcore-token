333dd5abe4a47aa287264edf4cb6c07e22de76f3
sachithsilva <sachithsilva2012@gmail.com>
2020-02-06 00:42:57 +0530

    static file serving

--- jwtToken/Startup.cs
+++ jwtToken/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
 
 namespace jwtToken
 {
@@ -25,6 +27,8 @@ namespace jwtToken
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt =>
+            opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
         }
 
@@ -41,7 +45,8 @@ namespace jwtToken
             app.UseRouting();
 
             app.UseAuthorization();
-
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
